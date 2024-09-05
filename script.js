46d74b66be8afeee82151b602e8eeb4303b8064c
LakshayKhatri9 <lk2417684@gmail.com>
2024-09-05 18:46:43 +0530

    table

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let project = document.querySelector(".project");
 let resi = document.querySelector(".resi");
 let ongoing = document.querySelector(".ongoing");
-let table = document.querySelector(".table-add")
+let table = document.querySelector(".table-add");
+let ongoing_del = document.querySelector(".ongoing-del");
+let table_2 = document.querySelector(".table-add-2");
+let del = document.querySelector(".del");
 
 project.addEventListener("click", () => {
     resi.classList.toggle("resi-hide");
 });
 
-
 resi.addEventListener("click",() => {
-    ongoing.classList.toggle("ongoing-hide");
+    ongoing_del.classList.toggle("ongoing-del-hide");
+})
+
+ongoing.addEventListener("click",() => {
+    table.classList.toggle("table-hide");
+    table_2.classList.add("table-hide-2")
 });
 
-
-ongoing.addEventListener("click", () => {
-    table.classList.toggle("table-hide")
-});
+del.addEventListener("click", () => {
+    table.classList.add("table-hide");
+    table_2.classList.toggle("table-hide-2")
+})
